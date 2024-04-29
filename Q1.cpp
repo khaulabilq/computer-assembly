a6feb131703a16603a3cd18f13e3eb4b660e0d1f
khaulabilq <168473177+khaulabilq@users.noreply.github.com>
2024-04-29 23:42:29 +0500

    Add files via upload

---
+++ Q1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include"Q1.h"
+using namespace std;
+
+// khaula bilqees, CS-F, 23i-0574, Marium hida, Muhammad Abdur Rafey
+
+int main() {
+	ComputerAssembly comp;
+	cout << "Do you want to build a PC or MAC: ";
+	string ty;
+	cin >> ty;
+	cin >> comp;
+
+	comp.display();
+
+
+}
